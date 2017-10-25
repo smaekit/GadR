fdf3896c9af63440c858ddc1fd922c03f6360dfb
brca1515 <brca1515@student.ju.se>
2017-10-25 14:55:03 +0200

    Added facebook login button

--- app/src/main/java/com/marcusjakobsson/gadr/MainActivity.java
+++ app/src/main/java/com/marcusjakobsson/gadr/MainActivity.java
@@ -1,16 +1,72 @@
 package com.marcusjakobsson.gadr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Test commit /carl
-    private String testCommitString = "Commit";
+    LoginButton loginButton;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Log.i("FB:", "Callback success!");
+                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                        
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Log.i("FB:", "Callback cancel!");
+                Toast.makeText(MainActivity.this, "Cancel", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Log.i("FB:", "Callback ERROR!");
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 }
+
+
